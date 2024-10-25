631a02538b6e2ce40863b2a8425db04f900cc47e
ramalalsgndrv <raliisgenderov@std.beu.edu.az>
2024-10-25 15:06:37 +0400

    first commit

---
+++ src/main/kotlin/org/example/hesen/HesenApplication.kt
@@ -0,0 +1,11 @@
+package org.example.hesen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HesenApplication
+
+fun main(args: Array<String>) {
+    runApplication<HesenApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/hesen/HesenApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.hesen
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HesenApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
